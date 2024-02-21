6cbea0bb446f6721aeb72a9bfecd07a0a0df0580
Maks Thimbarov <thimbarov@gmail.com>
2024-02-21 18:08:37 +0200

    redacted images folder

--- src/constants/Data.ts
+++ src/constants/Data.ts
@@ -13,30 +13,30 @@ export const mockData = [
  },
  {
   location: "Paris",
-  src: "/src/assets/images/cities/paris.jpg",
+  src: "/assets/images/cities/paris.jpg",
  },
  {
   location: "Rome",
-  src: "/src/assets/images/cities/rome.jpg",
+  src: "/assets/images/cities/rome.jpg",
  },
  {
   location: "Tokyo",
-  src: "/src/assets/images/cities/tokyo.jpg",
+  src: "/assets/images/cities/tokyo.jpg",
  },
  {
   location: "Sydney",
-  src: "/src/assets/images/cities/sydney.jpg",
+  src: "/assets/images/cities/sydney.jpg",
  },
  {
   location: "Berlin",
-  src: "/src/assets/images/cities/berlin.jpg",
+  src: "/assets/images/cities/berlin.jpg",
  },
  {
   location: "Barcelona",
-  src: "/src/assets/images/cities/barcelona.jpg",
+  src: "/assets/images/cities/barcelona.jpg",
  },
  {
   location: "Los Angeles",
-  src: "/src/assets/images/cities/los-angeles.jpg",
+  src: "/assets/images/cities/los-angeles.jpg",
  },
 ];
